7cbb357bb6c2ecee8a854a5a2b182f20d24e568a
stizzle123 <arifayanidowu2@yahoo.com>
2018-04-13 19:46:07 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Registering express middleware
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    next();
+});
+
+/* // Maintenance mode
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+}); */
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to Our Home page',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.status(404).send({
+        errorMessage: `404 Page not found`
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
